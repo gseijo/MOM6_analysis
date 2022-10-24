162542041e67e6baef541be0429c917bfcf93ee8
Giovanni Seijo-Ellis <gseijo@cheyenne5.cheyenne.ucar.edu>
2022-10-24 13:56:35 -0600

    add codes from Cheyenne

--- README.md
+++
@@ -1,2 +0,0 @@
-# MOM6_analysis
-Jupyter Notebooks and python scripts for MOM6 output analysis
---
+++ motu_options_parser.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = "Copernicus Marine User Support Team"
+__copyright__ = "(C) 2021 E.U. Copernicus Marine Service Information"
+__credits__ = ["E.U. Copernicus Marine Service Information"]
+__license__ = "MIT License - You must cite this source"
+__version__ = "202105"
+__maintainer__ = "D. Bazin, E. DiMedio, J. Cedillovalcarce, C. Giordan"
+__email__ = "servicedesk dot cmems at mercator hyphen ocean dot eu"
+
+def motu_option_parser(script_template, usr, pwd, output_filename):
+    dictionary = dict(
+        [e.strip().partition(" ")[::2] for e in script_template.split('--')])
+    dictionary['variable'] = [value for (var, value) in [e.strip().partition(" ")[::2] for e in script_template.split('--')] if var == 'variable']  # pylint: disable=line-too-long
+    for k, v in list(dictionary.items()):
+        if v == '<OUTPUT_DIRECTORY>':
+            dictionary[k] = '.'
+        if v == '<OUTPUT_FILENAME>':
+            dictionary[k] = output_filename
+        if v == '<USERNAME>':
+            dictionary[k] = usr
+        if v == '<PASSWORD>':
+            dictionary[k] = pwd
+        if k in ['longitude-min', 'longitude-max', 'latitude-min', 
+                 'latitude-max', 'depth-min', 'depth-max']:
+            dictionary[k] = float(v)
+        if k in ['date-min', 'date-max']:
+            dictionary[k] = v[1:-1]
+        dictionary[k.replace('-','_')] = dictionary.pop(k)
+    dictionary.pop('python')
+    dictionary['auth_mode'] = 'cas'
+    return dictionary
